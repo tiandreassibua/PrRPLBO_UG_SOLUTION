234ee99ff9756a73050065449ad1bad5f8fe648b
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-05 09:38:01 +0700

    Solution UG3 Tipe B Soal1

--- UG3/Unguided5_B/src/main/java/com/ug5b/soal1/SmartCard.java
+++ UG3/Unguided5_B/src/main/java/com/ug5b/soal1/SmartCard.java
@@ -1,7 +1,33 @@
 package com.ug5b.soal1;
 
 public class SmartCard {
+    private String nama;
+    private long saldo;
 
-    //TODO 1: Lengkapi atribut dan fungsi sesuai dengan class diagram SmartCard
+    void setNama(String nama) {
+        this.nama = nama;
+    }
 
+    String getNama() {
+        return this.nama;
+    }
+
+    void setSaldo(long nominal) {
+        if (nominal > 0) {
+            this.saldo = nominal;
+        }
+    }
+
+    long getSaldo() {
+        return this.saldo;
+    }
+
+    void topUp(Voucher vch) {
+        if ((vch.getKode()).equals("VC")) {
+            System.out.println("Kode Voucher: " + vch.getKode() + " || Top up gagal! Kode voucher tidak valid.");
+        } else {
+            this.saldo = vch.getNominal();
+            System.out.println("Kode Voucher: " + vch.getKode() + " || Top up sukses!");
+        }
+    }
 }
--- UG3/Unguided5_B/src/main/java/com/ug5b/soal1/Voucher.java
+++ UG3/Unguided5_B/src/main/java/com/ug5b/soal1/Voucher.java
@@ -1,6 +1,26 @@
 package com.ug5b.soal1;
 
 public class Voucher {
+    private String kode;
+    private long nominal;
 
-    //TODO 2: Lengkapi atribut dan fungsi sesuai dengan class diagram Voucher
+    void setNominal(long nominal) {
+        this.nominal = nominal;
+    }
+
+    String getKode() {
+        return this.kode;
+    }
+
+    long getNominal() {
+        return this.nominal;
+    }
+
+    void buatVoucher() {
+        if (this.nominal > 0) {
+            this.kode = "VC" + this.nominal;
+        } else {
+            this.kode = "VC";
+        }
+    }
 }
