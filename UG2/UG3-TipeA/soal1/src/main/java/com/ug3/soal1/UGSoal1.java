bb016748e37ffd6096fe1c7737971575a8cc61ce
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-05 08:28:15 +0700

    Solution UG2

--- UG2/UG3-TipeA/soal1/src/main/java/com/ug3/soal1/UGSoal1.java
+++ UG2/UG3-TipeA/soal1/src/main/java/com/ug3/soal1/UGSoal1.java
@@ -4,16 +4,32 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-public class UGSoal1
-{
-    public static void main( String[] args )
-    {
-        // panggil fungsi absen disini
+public class UGSoal1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nama : ");
+        String name = scanner.nextLine();
+
+        // get current date and time
+        // time ada pm dan am
+        // format tanggal contoh: 14-Februari-2024
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMMM-yyyy hh:mm:ssa");
+        Date date = new Date();
+        String now = formatter.format(date);
+
+        String[] splitNow = now.split(" ");
+
+        UGSoal1 soal1 = new UGSoal1();
+        soal1.absen(name, splitNow[0], splitNow[1]);
+
     }
 
     public void absen(String name, String date, String time) {
-        // tulis kode anda disini
+        System.out.println("berhasil absensi");
+        System.out.println("Atas nama : " + name);
+        System.out.println("Pada tanggal : " + date);
+        System.out.println("Pukul : " + time);
     }
 
-
-    }
+}
--- UG2/UG3-TipeA/soal2/src/main/java/com/ug3/soal2/UG3Soal2.java
+++ UG2/UG3-TipeA/soal2/src/main/java/com/ug3/soal2/UG3Soal2.java
@@ -1,19 +1,48 @@
 package com.ug3.soal2;
 
 import java.util.Scanner;
-public class UG3Soal2
-{
-    public static void main( String[] args ) {
 
-        // panggil fungsi fibonacci disini
+public class UG3Soal2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("Bilangan Fibonacci dari kata");
+
+            System.out.print("Masukkan input pertama: ");
+            String firstWord = scanner.nextLine();
+
+            System.out.print("Masukkan input kedua: ");
+            String secondWord = scanner.nextLine();
+
+            System.out.println("Bilangan Fibonacci");
+            fibonacci(firstWord, secondWord);
+
+            System.out.print("\nKeluar dari program (ketikan exit) : ");
+            String exit = scanner.nextLine();
+            if (exit.equals("exit")) {
+                isRunning = false;
+                scanner.close();
+            }
+        }
     }
 
     public static void fibonacci(
             String firstWord,
             String secondWord
 
-    ){
-        // tulis kode anda disini
+    ) {
+        int n = 11;
+        int firstNumber = firstWord.length();
+        int secondNumber = secondWord.length();
+        int nextNumber;
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstNumber + " ");
+            nextNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+        }
     }
 
 }
