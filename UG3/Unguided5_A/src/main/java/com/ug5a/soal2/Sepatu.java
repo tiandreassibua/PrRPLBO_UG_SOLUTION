32cb8c02d30d3be89c0c5ea98a53121c2168fe1a
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-05 09:07:36 +0700

    Solution UG3 Tipe A Soal2

--- UG3/Unguided5_A/src/main/java/com/ug5a/soal2/Sepatu.java
+++ UG3/Unguided5_A/src/main/java/com/ug5a/soal2/Sepatu.java
@@ -1,6 +1,40 @@
 package com.ug5a.soal2;
 
 public class Sepatu {
-    // TODO 1: Lengkapi attribut dan fungsi kelas ini sesuai dengan class diagram Sepatu
+    private String kode;
+    private static int nextNum = 1;
+    private String merkModel;
+    private int ukuran;
+    private long harga;
+    private int stok;
 
+    public Sepatu(String argMerkModel, int argUkuran, long argHarga, int argStok) {
+        this.kode = "NK0024510" + nextNum;
+        nextNum++;
+
+        this.merkModel = argMerkModel;
+        this.ukuran = argUkuran;
+        this.harga = argHarga;
+        this.stok = argStok;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getMerkModel() {
+        return merkModel;
+    }
+
+    public int getUkuran() {
+        return ukuran;
+    }
+    
+    public long getHarga() {
+        return harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
 }
